4724c0b61f7a35c7091cfe47b4db142a8a89e1fd
Stas Makievsky <makievskij@mirea.ru>
2024-09-16 20:51:57 +0300

    first commit

---
+++ WpfAppLecture1/ClassApp/ClassNav.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfAppLecture1.ClassApp
+{
+    internal class ClassNav
+    {
+        public static Frame frmContent { get; set; }    
+    }
+}
---
+++ WpfAppLecture1/PagesApp/PageMain.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfAppLecture1.ClassApp;
+
+namespace WpfAppLecture1.PagesApp
+{
+    /// <summary>
+    /// Логика взаимодействия для PageMain.xaml
+    /// </summary>
+    public partial class PageMain : Page
+    {
+        public PageMain()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (TxbLogin.Text == "123" && PsbPass.Password == "123")
+                {
+                    ClassNav.frmContent.Navigate(new PageContent());
+                }
+                else
+                {
+                    MessageBox.Show("Пароль неверный!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+    }
+}
